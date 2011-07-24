03343076a407c192ca035e0f89c9bd0d68820dde
Steven Fusco <github@stevenfusco.com>
2011-07-24 00:33:46 -0400

    renaming app delegate with ObjC++ extension

--- KXNT/KXNTAppDelegate.mm
+++ KXNT/KXNTAppDelegate.mm
@@ -40,6 +40,7 @@
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
+    [self.mainViewController enterBackground];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -47,6 +48,7 @@
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
+    [self.mainViewController enterForground];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
