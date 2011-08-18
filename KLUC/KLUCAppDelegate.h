22cf9e077c2c8bbb597d8a08964a7b21f46626ba
Steven Fusco <github@stevenfusco.com>
2011-08-18 16:42:21 -0400

    kluc adhoc build 2011-08-18

---
+++ KLUC/KLUCAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  KLUCAppDelegate.h
+//  KLUC
+//
+//  Created by Steven Fusco on 7/23/11.
+//  Copyright 2011 Cibo Technology, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface KLUCAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet MainViewController *mainViewController;
+
+@end
